60612d0032ff0582f0b3a0a3fe541990ed9c76ef
Aleksandr Paklin <paklin@profiterole.group>
2023-09-03 11:25:03 +0500

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+const API_TOKEN = process.env.API_TOKEN;
+const API_URL = process.env.API_URL;
 
-export default function Home() {
-  return <div>NEXT-STRAPI-DOCKER-TEST</div>;
+export default async function Home() {
+  const res = await fetch(`${API_URL}/api/news?populate=preview`, {
+    headers: {
+      authorization: `bearer ${API_TOKEN}`,
+    },
+  });
+
+  const { data } = await res.json();
+
+  return (
+    <div className="flex p-5 flex-col gap-4 ">
+      {/* @ts-ignore */}
+      {data.map((item) => (
+        <div key={item.id} className="border border-gray-400 p-5 rounded max-w-[400px]">
+          <div>{item.attributes.title}</div>
+          <div>{item.attributes.description}</div>
+          <img src={item.attributes.preview.data.attributes.url} />
+        </div>
+      ))}
+    </div>
+  );
 }
